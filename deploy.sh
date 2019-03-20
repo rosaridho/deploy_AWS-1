c2c53b575978e271c57b0781aa53aaa00af1704a
rosaridho <muhammadridhorosa@gmail.com>
2019-03-20 15:59:38 +0700

    port 5002 salah

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password
 sudo docker stop test_project
 sudo docker rm test_project
 sudo docker rmi ridhorosa/ecommerce2:production3
-sudo docker run -d --name test_project -p 5002:5002 ridhorosa/ecommerce2:production3
+sudo docker run -d --name test_project -p 5000:5000 ridhorosa/ecommerce2:production3
