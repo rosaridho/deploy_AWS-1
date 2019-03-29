4ae1fac10863b25a69eb7e9c6096e72513a09e24
rosaridho <muhammadridhorosa@gmail.com>
2019-03-29 22:01:49 +0700

    revisi2

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git pull
 
 source ~/.profile
 echo "$DOCKERHUB_PASS" | sudo docker login --username $DOCKERHUB_USER --password-stdin
-sudo docker stop ridhorosa/ecommerce2:production3
-sudo docker rm ridhorosa/ecommerce2:production3
+sudo docker stop ridhorosa
+sudo docker rm ridhorosa
 sudo docker rmi ridhorosa/ecommerce2:production3
-sudo docker run -d --name ridhorosa/ecommerce2:production3 -p 5000:5000 ridhorosa/ecommerce2:production3
+sudo docker run -d --name ridhorosa -p 5000:5000 ridhorosa/ecommerce2:production3
